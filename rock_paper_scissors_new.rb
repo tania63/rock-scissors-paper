64ec85abac26e8441c986fd20325826ce6fae927
tania63 <tindjova@abv.bg>
2014-06-13 21:21:30 +0300

    update pc choice

--- rock_paper_scissors_new.rb
+++ rock_paper_scissors_new.rb
@@ -1,19 +1,12 @@
 puts "Rock, paper, scissors?"
 user = gets.chomp
-randnumber = 1 + rand(2)
-if randnumber == 1
-	pc = "rock"
-elsif randnumber == 2
-	pc = "paper"
-elsif randnumber == 3
-	pc = "scissors"
-end
+pc = [:rock, :paper, :scissors]
 
 h = {scissors: :paper, rock: :scissors, paper: :rock} 
 if h.keys.include? user.to_sym
-	if user.to_sym == pc.to_sym
+	if user.to_sym == pc
 		puts "PC choice #{pc}, It's a tie"
-	elsif h[user.to_sym] == pc.to_sym
+	elsif h[user.to_sym] == pc
 		puts "PC choice #{pc}, You win"
 	else 
 		puts "PC choice #{pc}, You lose"
